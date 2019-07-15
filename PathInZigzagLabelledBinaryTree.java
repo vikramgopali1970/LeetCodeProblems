ebf32e34e8b606f45419640833fc2ee386ededc5
Vikram Gopali <vikramgopali.93@gmail.com>
2019-07-14 17:53:55 -0700

    Path In Zig Zag Labelled Binary Tree

--- PathInZigzagLabelledBinaryTree.java
+++ PathInZigzagLabelledBinaryTree.java
@@ -1,4 +1,41 @@
 package LeetProblem;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class PathInZigzagLabelledBinaryTree {
+    private int getLevel(int label){
+        int n=1, cur = 1;
+        while(label >= cur){
+            n++;
+            cur = (int)Math.pow(2,(n-1));
+        }
+        return n-1;
+    }
+
+    private int getParent(int child, int level){
+        return this.getMirrorElement(child/2,level-1);
+    }
+
+    private int getMirrorElement(int node, int level){
+        int totalNodes = (int)Math.pow(2,level-1);
+        int pos = node - totalNodes;
+        int mirrPos = totalNodes - pos;
+        return totalNodes + mirrPos - 1;
+    }
+
+    public List<Integer> pathInZigZagTree(int label) {
+        List<Integer> res = new ArrayList<Integer>();
+        int level = this.getLevel(label);
+        res.add(label);
+        int parent = getParent(label,level);
+        while(parent >= 1){
+            res.add(parent);
+            level--;
+            parent = this.getParent(parent,level);
+        }
+        Collections.reverse(res);
+        return res;
+    }
 }
