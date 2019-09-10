68c479890fed3ddf2b801320bfc4bf50e77f2674
Vikram Gopali <vikramgopali.93@gmail.com>
2019-09-10 13:25:54 -0500

    MinimumCostToConnectStick

--- MinimumCostToConnectStick.java
+++ MinimumCostToConnectStick.java
@@ -1,4 +1,20 @@
 package LeetProblem;
 
+import java.util.PriorityQueue;
+
 public class MinimumCostToConnectStick {
+    public int connectSticks(int[] sticks){
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        for(int i=0;i<sticks.length;i++){
+            pq.offer(sticks[i]);
+        }
+        int res=0;
+        while(pq.size() != 1){
+            int s1 = pq.poll();
+            int s2 = pq.poll();
+            res+=s1+s2;
+            pq.offer(s1+s2);
+        }
+        return res;
+    }
 }
