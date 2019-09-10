80d1bb7cc251f4362895d1cf6cbf6a38d68b8de8
Vikram Gopali <vikramgopali.93@gmail.com>
2019-09-10 13:26:10 -0500

    NumSmallerByFrequency

--- NumSmallerByFrequency.java
+++ NumSmallerByFrequency.java
@@ -1,4 +1,43 @@
 package LeetProblem;
 
+import java.util.TreeSet;
+
 public class NumSmallerByFrequency {
+    public int[] numSmallerByFrequency(String[] queries, String[] words) {
+        TreeSet<Integer> qset = new TreeSet<Integer>();
+        TreeSet<Integer> wset = new TreeSet<Integer>();
+        for(int i=0;i<words.length;i++){
+            int[] f = new int[26];
+            for(int j=0;j<words[i].length();j++){
+                char c = words[i].charAt(j);
+                f[c-'a']++;
+            }
+            for(int j=0;j<26;j++){
+                if(f[j] > 0){
+                    wset.add(f[j]);
+                    break;
+                }
+            }
+        }
+        int[] q = new int[queries.length];
+        int[] res = new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            int[] f = new int[26];
+            for(int j=0;j<queries[i].length();j++){
+                char c = queries[i].charAt(j);
+                f[c-'a']++;
+            }
+            for(int j=0;j<26;j++){
+                if(f[j] > 0){
+                    q[i] = f[j];
+                    break;
+                }
+            }
+        }
+        for(int i=0;i<q.length;i++){
+            res[i] = wset.headSet(q[i]).size();
+        }
+        System.out.println(wset);
+        return res;
+    }
 }
